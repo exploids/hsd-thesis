43c3cab0561b07945fb0b6293bbfba879e27a813
Luca Selinski <luca.selinski@valtech.com>
2023-04-03 19:36:05 +0200

    add support for plural acronyms

--- data/acronyms.lua
+++ data/acronyms.lua
@@ -1,35 +1,33 @@
 local meta_before
-local vars = {}
+local acronyms = {}
 
 local function load_acronyms(meta)
-	local acronyms = meta.acronyms
-	if acronyms ~= nil then
-		for _, acronym in ipairs(acronyms) do
-			vars[pandoc.utils.stringify(acronym.short)] = { table.unpack(acronym.long) }
+	local meta_acronyms = meta.acronyms
+	if meta_acronyms ~= nil then
+		for index, acronym in ipairs(meta_acronyms) do
+			local short = pandoc.utils.stringify(acronym.short)
+			local name = acronym.name or short:gsub("%W", ""):lower() .. index
+			acronym.name = name
+			acronyms[short] = { name = name, plural = false }
+			if acronym["short-plural"] then
+				acronyms[pandoc.utils.stringify(acronym["short-plural"])] = { name = name, plural = true }
+			end
 		end
 	end
 	meta_before = meta
 end
 
 local function replace(el)
-	local text = el.text
-	local prefix = nil
-	local suffix = nil
-	if text:sub(1, 1) == '(' then
-		text = text:sub(2)
-		prefix = '('
-	end
-	if text:sub(-1) == ')' then
-		text = text:sub(1, -2)
-		suffix = ')'
-	end
-	if vars[text] then
+	local prefix, text, suffix = el.text:match "^(%p*)(%w+)(%p*)$"
+	local acronym = acronyms[text]
+	if acronym then
 		local result = {}
-		if prefix then
+		if #prefix > 0 then
 			table.insert(result, prefix)
 		end
-		table.insert(result, pandoc.RawInline("latex", "\\ac{" .. text .. "}"))
-		if suffix then
+		local command = (acronym.plural and "\\acp{" or "\\ac{") .. acronym.name .. "}"
+		table.insert(result, pandoc.RawInline("latex", command))
+		if #suffix > 0 then
 			table.insert(result, suffix)
 		end
 		return result
