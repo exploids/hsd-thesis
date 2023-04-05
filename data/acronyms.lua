e4378aafd5e1b3058a0ad9d01f0bd827981b19c6
Luca Selinski <luca.selinski@posteo.de>
2023-04-05 19:36:38 +0200

    add logic for determining the longest acronym

--- data/acronyms.lua
+++ data/acronyms.lua
@@ -1,9 +1,14 @@
 local meta_before
 local acronyms = {}
 
+local function compare_acronyms(a, b)
+	return pandoc.utils.stringify(a.short):upper() < pandoc.utils.stringify(b.short):upper()
+end
+
 local function load_acronyms(meta)
 	local meta_acronyms = meta.acronyms
 	if meta_acronyms ~= nil then
+		local longest = ""
 		for index, acronym in ipairs(meta_acronyms) do
 			local short = pandoc.utils.stringify(acronym.short)
 			local name = acronym.name or short:gsub("%W", ""):lower() .. index
@@ -12,7 +17,14 @@ local function load_acronyms(meta)
 			if acronym["short-plural"] then
 				acronyms[pandoc.utils.stringify(acronym["short-plural"])] = { name = name, plural = true }
 			end
+			if #short > #longest then
+				longest = short
+			end
 		end
+		if not meta["acronyms-longest"] then
+			meta["acronyms-longest"] = longest
+		end
+		table.sort(meta_acronyms, compare_acronyms)
 	end
 	meta_before = meta
 end
